b6dcc237b124d3744513badadf597a1386983688
Oliver Chen <xoliverchen@gmail.com>
2026-01-22 00:47:29 -0700

    🛠️ Fix dubious ownership errors

--- dev.sh
+++ dev.sh
@@ -11,6 +11,6 @@ container_name="dev-$1"
 if podman container exists "$container_name"; then
     podman exec -it "$container_name" zsh
 else
-    podman run -e DISPLAY=$DISPLAY -e XDG_RUNTIME_DIR=/tmp -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/$WAYLAND_DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/workspace --name "$container_name" -h "$container_name" -v $HOME/.config/opencode/:/home/dev/.config/opencode/ -v "$1_home_data:/home/dev/" --network=host -itd dev
+    podman run -e DISPLAY=$DISPLAY -e XDG_RUNTIME_DIR=/tmp -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/$WAYLAND_DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --name "$container_name" -h "$container_name" -v $PWD:/workspace:U -v $HOME/.config/opencode/:/home/dev/.config/opencode/:U -v "$1_home_data:/home/dev/" --network=host -itd dev
     podman exec -it "$container_name" zsh
 fi
