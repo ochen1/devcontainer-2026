00bafabed1f1f40a551fc66bf76ea1128650c7a0
Oliver Chen <xoliverchen@gmail.com>
2026-01-22 20:18:04 -0700

    🧑 Make user id mapping system more robust

--- dev.sh
+++ dev.sh
@@ -13,6 +13,6 @@ extra_opts=("$@")
 if podman container exists "$container_name"; then
     podman exec -it "$container_name" zsh
 else
-    podman run -e DISPLAY=$DISPLAY -e XDG_RUNTIME_DIR=/tmp -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/$WAYLAND_DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --name "$container_name" -h "$container_name" --add-host "$container_name:127.0.0.1" -v $PWD:/workspace:U -v $HOME/.config/opencode/:/home/dev/.config/opencode/:U -v "${container_name}_home_data:/home/dev/" --network=host "${extra_opts[@]}" -itd dev
+    podman run -e DISPLAY=$DISPLAY -e XDG_RUNTIME_DIR=/tmp -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/$WAYLAND_DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --name "$container_name" -h "$container_name" --add-host "$container_name:127.0.0.1" -v $PWD:/workspace:U -v $HOME/.config/opencode/:/home/dev/.config/opencode/:U -v "${container_name}_home_data:/home/dev/" --network=host --user 1000:1000 --userns=keep-id:uid=1000,gid=1000 "${extra_opts[@]}" -itd dev
     podman exec -it "$container_name" zsh
 fi
